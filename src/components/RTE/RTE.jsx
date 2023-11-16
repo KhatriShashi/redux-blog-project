e05713bdb5838462e06b2a98be7856baaf927c1e
KhatriShashi <shashikrkhatri9546@gmail.com>
2023-11-16 11:50:51 +0530

    changes in RTE

--- src/components/RTE/RTE.jsx
+++ src/components/RTE/RTE.jsx
@@ -47,7 +47,35 @@ function RTE({
                                 "undo redo | formatselect | bold italic backcolor forecolor removeformat | \
                     alignleft aligncenter alignright alignjustify | \
                     bullist numlist outdent indent | link image media table | help",
-                            content_style: "body { font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px}"
+                            content_style: "body { font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px}",
+                            mobile: {
+        theme: "mobile",
+        plugins: [
+            "image",
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "charmap",
+            "image",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "paste",
+            "wordcount",
+            "help"
+        ],
+        toolbar: "undo redo | formatselect | bold italic backcolor forecolor removeformat | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | link image media table | help",
+                content_style: "body { font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px}",
+    }
                         }}
                         onEditorChange={onChange}
                     />
