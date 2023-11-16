e5a1db8ed38b8fa5d55be1bb5f3e088fa3a5fb22
KhatriShashi <shashikrkhatri9546@gmail.com>
2023-11-16 07:12:29 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Login from "./authentication/Login";
 import SignUp from "./authentication/SignUp";
 import YourBlog from "./blog/yourBlog";
 import Home from "./blog/Home";
-import Button from "./button";
+import Button from "./button/Button"
 import RTE from "./RTE/RTE";
 import Input from "./input/Input";
 import BlogPage from "./card/BlogPage";
